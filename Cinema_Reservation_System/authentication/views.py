3bd61ffcde9399e1a97d640a90fa0ff3ac2b4eff
Mehdi-0810 <ladjelatemehdi@gmail.com>
2023-07-27 17:46:37 +0100

    changes

--- Cinema_Reservation_System/authentication/views.py
+++ Cinema_Reservation_System/authentication/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render, redirect
 from .forms import SignUpForm, SignInForm
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+import uuid
+from .models import UserProfile
+from django.core.mail import send_mail
+
+
+
 
 def sign_in_view(request):
     if request.method == 'POST':
@@ -21,21 +28,52 @@ def sign_in_view(request):
 
     return render(request, 'signin.html', {'form': form})
 
-
+@login_required
 def sign_up_view(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
 
-            return redirect('signin')
+            # Generate a verification token
+            verification_token = str(uuid.uuid4())
+            user.email_verification_token = verification_token
+            
+            user.save()
+
+            # Send the verification email
+            subject = 'Email Verification'
+            message = f'Click the link below to verify your email:\n\n' \
+                      f'http://example.com/verify_email/{verification_token}/'
+            from_email = 'noreply@gmail.com'
+            to_email = user.email
+            send_mail(subject, message, from_email, [to_email])
+
+            return redirect('email_verification_pending')
     else:
         form = SignUpForm()
 
     return render(request, 'signup.html', {'form': form})
 
 
+def email_verification_pending(request):
+    return render(request, 'email_verification_pending.html')
 
+
+
+def verify_email(request, verifiction_token):
+    try:
+        user = UserProfile.objects.get(email_verified=False)
+        if user.email_verification_token == verifiction_token:
+            user.email_verified = True
+            user.save()
+            return render(request, 'email_verified.html')
+        else:
+            return render(request, 'email_verification_invalid.html')
+    
+    except UserProfile.DoesNotExist:
+        return render(request, 'email_verification_invalid.html')
 
 
 
