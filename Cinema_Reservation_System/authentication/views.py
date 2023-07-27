09049bd785ede125d525e32d0b8e8d88975f80ec
YCN <89133149+sifyacine@users.noreply.github.com>
2023-07-27 14:20:48 +0100

    changes

--- Cinema_Reservation_System/authentication/views.py
+++ Cinema_Reservation_System/authentication/views.py
@@ -1,17 +1,14 @@
 from rest_framework.generics import CreateAPIView
 from .models import UserProfile
 from .serializers import UserProfileSerializer, SignInSerializer
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.models import User
-from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import status
-
+from django.contrib.auth.hashers import check_password
 
 class UserProfileCreateView(CreateAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
-
 
 class SignInView(APIView):
     def post(self, request):
@@ -19,24 +16,21 @@ class SignInView(APIView):
         if serializer.is_valid():
             email_or_phone = serializer.validated_data['email_or_phone']
             password = serializer.validated_data['password']
-
-            # Try to fetch user based on email
             try:
+                # Retrieve the user based on the provided email
                 user_profile = UserProfile.objects.get(email=email_or_phone)
-
-                # Authenticate the user based on fetched user object
-                user = authenticate(request, username=user_profile.email, password=password)
-
-                if user is not None:
-                    # User is authenticated, perform login and return success response
-                    login(request, user)
-                    return Response({"message": "User authenticated successfully."})
-
+                if user_profile.email == email_or_phone or user_profile.phone_number == email_or_phone:
+                    if user_profile.password == password:
+                        return Response({"message": "access accepted."})  
+                    else:
+                        return Response({"message": "wrong password."})  
+                else:
+                    return Response({"message": "wrong email or phone number."})
+                          
             except UserProfile.DoesNotExist:
-                pass
+                # User not found, return error response
+                return Response({"message": "Invalid credentials."}, status=status.HTTP_401_UNAUTHORIZED)
 
-            # User authentication failed, return error response
-            return Response({"message": "Invalid credentials."}, status=status.HTTP_401_UNAUTHORIZED)
         else:
             # Invalid input data, return error response
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
