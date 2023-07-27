270affedb35b167aacafb740871f1991019df518
YCN <89133149+sifyacine@users.noreply.github.com>
2023-07-27 16:09:36 +0100

    changes

--- Cinema_Reservation_System/authentication/admin.py
+++ Cinema_Reservation_System/authentication/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import UserProfile
 
-# Register your models here.
+class UserProfileAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'phone_number')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('email', 'first_name', 'last_name', 'phone_number')
+    ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'phone_number')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.register(UserProfile, UserProfileAdmin)
